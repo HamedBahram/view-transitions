40f48becba2c0299c2f4d6d4aa223a0a4c69871f
Hamed Bahram <hamed.bahram@gmail.com>
2024-04-19 15:59:07 -0400

    Add more pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,15 @@
-export default function Home() {
+export default function About() {
   return (
     <section className='py-12'>
       <div className='container'>
-        <h1 className='text-3xl font-bold'>View Transitions API</h1>
+        <h2 className='text-3xl font-bold'>About Us</h2>
         <div className='mt-12 flex gap-12'>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, sit
+            accusamus tenetur iure laboriosam ab dolor autem mollitia
+            perspiciatis enim est sint facere temporibus tempora dolorem.
+            Laborum veniam officiis eligendi.
+          </p>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, sit
             accusamus tenetur iure laboriosam ab dolor autem mollitia
--- components/header.tsx
+++ components/header.tsx
@@ -9,6 +9,12 @@ export default function Header() {
           <li>
             <Link href='/'>Home</Link>
           </li>
+          <li>
+            <Link href='/about'>About</Link>
+          </li>
+          <li>
+            <Link href='/contact'>Contact</Link>
+          </li>
         </ul>
 
         <ThemeToggle />
